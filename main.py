6f998b10c4c0c46d37f174b27263cfc3466de608
Prieto <MPrieto1@cps.k12.il.us>
2025-04-04 12:00:25 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# print("I like pizza!")
+# print("It's really good!")
+
+first_name= "Lucy Wright"
+# print(first_name)
+
+first_name = "Bro"
+food = "pizza"
+# f-string allows you to insert a variable
+# into a string
+print(f"Hello {first_name}")
+print(f"You like {food} likes {food}") 
