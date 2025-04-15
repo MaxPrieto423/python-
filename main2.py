75fe3d32bf8ad5cce1ff0feda22fa5abf693380d
Prieto <MPrieto1@cps.k12.il.us>
2025-04-15 11:47:49 -0500

    conditionals

--- main2.py
+++ main2.py
@@ -31,5 +31,48 @@ print(f"{name} is a student: {is_student}")
 # 1. create 4 differnet types of variables
 # 2. print them out
 # 3. use f strings to format the strings
-school1 = 
-print(f"{dog} and {cats} have {num1 + num2} owners")
+
+
+# if conditionals
+# if statements are used to check if a condition is ture or false
+# if condition is true, do something
+# if condition is false, do something else
+
+# if conditional statements always start with if
+# and depend on a boolean value (true or false)
+# example
+classStarted = True #boolean variable
+if classStarted:
+    print("class has started")
+else:
+    print("class has not started")
+
+# logical abd comparison operators
+# == equal to
+# != not equal to
+# > greater then
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+# and logical operators
+# or logical operators
+# not logical operators
+# example
+age = 18
+if age >= 18:
+    print("you are an adult")
+elif age == 17:
+    print("you are almost an adult")
+else:
+    print("you are a minor")
+
+# problem set #2
+# 1. create a program tgar checks if number
+# is even or odd
+# 2. ask user for a number
+number = int(input("what is your number"))
+# 3. check if number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
